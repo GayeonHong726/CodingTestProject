d4a482b0be3659155f2d9ee84a59f3614458c29c
GayeonHong726 <1379hong@naver.com>
2024-07-20 23:17:58 +0900

    [24.7.20] Programmers172928(2)

--- src/basics/Programmers172928.java
+++ src/basics/Programmers172928.java
@@ -7,16 +7,43 @@ import java.util.Scanner;
 public class Programmers172928 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        List<String> arr = new ArrayList<>();
+
+        int[] arr = {1,2,3};
+        System.out.println(arr.length);
+
+        List<String> park = new ArrayList<>();
+        List<String> routes = new ArrayList<>();
+
+        List<Integer> noX = new ArrayList<>();
+        List<Integer> noY = new ArrayList<>();
+        int startX = 0;
+        int startY = 0;
 
         for(int i = 0; i < 3; i++){
-            arr.add(sc.next());
+            park.add(sc.next());
         }
 
-        System.out.println(arr.size());
+        for(int i = 0; i < 3; i++){
+            routes.add(sc.next());
+        }
 
-        System.out.println("indexOf(soo) : " + arr.indexOf("soo"));
+        int x = park.get(0).length();
+        int y = park.size();
 
-        System.out.println(arr.toString());
+        for(int i = 0; i < x; i++) {
+            for(int j = 0; j < y; j++){
+                if(park.get(j).charAt(i) == 's'){
+                    startX = j;
+                    startY = i;
+                }
+                if(park.get(j).charAt(i) == 'x'){
+                    noX.add(j);
+                    noY.add(i);
+                }
+            }
+        }
+        System.out.println();
+
+
     }
 }
