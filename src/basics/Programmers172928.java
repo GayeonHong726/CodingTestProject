0b8bb192265acbc3b0cf1f7349914da18e22ce70
GayeonHong726 <1379hong@naver.com>
2024-07-21 15:35:56 +0900

    [24.7.20] Programmers172928(3)

--- src/basics/Programmers172928.java
+++ src/basics/Programmers172928.java
@@ -1,49 +1,106 @@
 package basics;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Programmers172928 {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
-        int[] arr = {1,2,3};
-        System.out.println(arr.length);
+        String[] park = {"SOO","OXX","OOO"};
+        String[] routes = {"E 2","S 2","W 1"};
+        System.out.println(Arrays.toString(solution(park,routes)));
+    }
 
-        List<String> park = new ArrayList<>();
-        List<String> routes = new ArrayList<>();
+    static public int[] solution(String[] park, String[] routes) {
+        int[] answer = new int[2];
 
-        List<Integer> noX = new ArrayList<>();
-        List<Integer> noY = new ArrayList<>();
-        int startX = 0;
-        int startY = 0;
+        String[][] arr = new String[park.length][park[0].length()];
 
-        for(int i = 0; i < 3; i++){
-            park.add(sc.next());
+        for ( int i =0; i<park.length; i++) {
+            for ( int j = 0; j < park[0].length(); j++) {
+                arr[i][j] = String.valueOf(park[i].charAt(j));
+            }
         }
 
-        for(int i = 0; i < 3; i++){
-            routes.add(sc.next());
-        }
+        int x = 0;
+        int y = 0;
 
-        int x = park.get(0).length();
-        int y = park.size();
-
-        for(int i = 0; i < x; i++) {
-            for(int j = 0; j < y; j++){
-                if(park.get(j).charAt(i) == 's'){
-                    startX = j;
-                    startY = i;
-                }
-                if(park.get(j).charAt(i) == 'x'){
-                    noX.add(j);
-                    noY.add(i);
+        for(int i = 0; i < park.length; i++){
+            for(int j = 0; j < park[0].length(); j++) {
+                if(arr[i][j].equals("S")) {
+                    x = i;
+                    y = j;
                 }
             }
         }
-        System.out.println();
 
+        for (String route : routes) {
+            // StringTokenizer는 자바에서 문자열을 토큰으로 분리할 때 사용하는 클래스
+            StringTokenizer st = new StringTokenizer(route);
+            String direction = st.nextToken();
+            int len = Integer.parseInt(st.nextToken());
 
+            switch ( direction ) {
+                case "E" :
+                    if (y + len < park[0].length()) {
+                        boolean blocked = false;
+                        for (int j = y + 1; j <= y + len; j++) {
+                            if (arr[x][j].equals("X")) {
+                                blocked = true;
+                                break;
+                            }
+                        }
+                        if (!blocked) {
+                            y += len;
+                        }
+                    }
+                    break;
+                case "W" :
+                    if (y - len >= 0) {
+                        boolean blocked = false;
+                        for (int j = y - 1; j >= y - len; j--) {
+                            if (arr[x][j].equals("X")) {
+                                blocked = true;
+                                break;
+                            }
+                        }
+                        if (!blocked) {
+                            y -= len;
+                        }
+                    }
+                    break;
+                case "S" :
+                    if (x + len < park.length) {
+                        boolean blocked = false;
+                        for (int j = x + 1; j <= x + len; j++) {
+                            if (arr[j][y].equals("X")) {
+                                blocked = true;
+                                break;
+                            }
+                        }
+                        if (!blocked) {
+                            x += len;
+                        }
+                    }
+                    break;
+                case "N" :
+                    if (x - len >= 0) {
+                        boolean blocked = false;
+                        for (int j = x - 1; j >= x - len; j--) {
+                            if (arr[j][y].equals("X")) {
+                                blocked = true;
+                                break;
+                            }
+                        }
+                        if (!blocked) {
+                            x -= len;
+                        }
+                    }
+                    break;
+            }
+        }
+        answer[0] = x;
+        answer[1] = y;
+
+        return answer;
     }
 }
