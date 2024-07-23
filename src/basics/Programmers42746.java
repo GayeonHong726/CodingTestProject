08978f3a59fd858efc4e89f29176486fd55fbe3f
GayeonHong726 <1379hong@naver.com>
2024-07-23 16:46:21 +0900

    [24.7.23] Programmers42746(2)

--- src/basics/Programmers42746.java
+++ src/basics/Programmers42746.java
@@ -1,4 +1,23 @@
 package basics;
 
+import java.util.*;
 public class Programmers42746 {
+    public static void main(String[] args) {
+        int[] numbers = {6, 10, 2};
+
+        String[] arr = new String[numbers.length];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.toString(numbers[i]);
+        }
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        if(arr[0].equals("0") && arr[arr.length-1].equals("0")){
+            System.out.println("0");
+        }else {
+            String result = String.join("",arr);
+            System.out.println(result);
+        }
+    }
 }
