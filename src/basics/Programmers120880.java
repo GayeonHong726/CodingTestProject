589b6813682077da9a98f82d53604b4f613e428c
GayeonHong726 <1379hong@naver.com>
2024-07-29 22:28:21 +0900

    [24.7.29] Programmers120880(2)

--- src/basics/Programmers120880.java
+++ src/basics/Programmers120880.java
@@ -1,12 +1,22 @@
 package basics;
 
-public class Programmers120880 {
-    public static void main(String[] args){
-        int n = 4;
-        int[] arr = {1, 2, 3, 4, 5, 6};
+import java.util.*;
 
-//        PriorityQueue<Integer>
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        Arrays.sort(numlist);
 
+        int len = numlist.length;
 
+        for ( int i = 0; i < len; i++) {
+            for( int j = 0; j < len; j++) {
+                if(Math.abs(n-numlist[i]) <= Math.abs(n-numlist[j])) {
+                    int temp = numlist[i];
+                    numlist[i] = numlist[j];
+                    numlist[j] = temp;
+                }
+            }
+        }
+        return numlist;
     }
 }
