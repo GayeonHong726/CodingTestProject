e9411f74a3b249950cd645f8e5e5a7e42d42c07c
GayeonHong726 <1379hong@naver.com>
2024-08-07 09:41:19 +0900

    [24.8.3] Programmers181847

---
+++ src/basics/Programmers181847.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class Programmers181847 {
+    class Solution {
+        public String solution(String n_str) {
+            String answer = "";
+
+            int num = Integer.parseInt(n_str);
+            answer = Integer.toString(num);
+
+            return answer;
+        }
+    }
+}
