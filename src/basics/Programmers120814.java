05db6fad6e2397dbf0ddf28eb7710f093ba29695
GayeonHong726 <1379hong@naver.com>
2024-08-09 13:59:20 +0900

    [24.8.9] Programmers120814

---
+++ src/basics/Programmers120814.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class Programmers120814 {
+    class Solution {
+        public int solution(int n) {
+            return (n % 7) == 0 ? n / 7 : n / 7 +1;
+        }
+    }
+}
