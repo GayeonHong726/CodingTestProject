571e91050925ec3911944721c36e5820172cd422
GayeonHong726 <1379hong@naver.com>
2024-08-16 01:03:10 +0900

    [24.8.15] Programmars293261

---
+++ src/basics/Programmars293261.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class Programmars293261 {
+//    SELECT fi.ID 'ID', fni.FISH_NAME 'FISH_NAME', fi.LENGTH 'LENGTH'
+//    FROM FISH_INFO fi, FISH_NAME_INFO fni
+//    WHERE fi.FISH_TYPE=fni.FISH_TYPE
+//    AND fi.LENGTH IN (SELECT MAX(LENGTH) FROM FISH_INFO GROUP BY FISH_TYPE)
+//    ORDER BY fi.ID
+}
