959bd9c32e24f819650efabc7fa2e4ef290b5253
GayeonHong726 <1379hong@naver.com>
2024-08-19 14:44:10 +0900

    [24.8.17] Programmers12932

---
+++ src/basics/Programmers12932.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class Programmers12932 {
+    class Solution {
+        public int[] solution(long n) {
+
+            String s ="" + n;
+
+            int[] answer = new int[s.length()];
+
+            int num = 0;
+            while(n > 0) {
+                answer[num] = (int)(n % 10);
+                n /= 10;
+                num++;
+            }
+            return answer;
+        }
+    }
+}
