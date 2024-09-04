d3b9ffb55ec1a9998db97e90b73274c28e98aedb
GayeonHong726 <1379hong@naver.com>
2024-09-04 14:54:27 +0900

    [24.9.3] Programmars160586

---
+++ src/basics/Programmars160586.java
@@ -0,0 +1,38 @@
+package basics;
+
+import java.util.*;
+public class Programmars160586 {
+    class Solution {
+        public int[] solution(String[] keymap, String[] targets) {
+            int[] answer = new int[targets.length];
+            HashMap<Character, Integer> map = new HashMap<>();
+
+            for(int i = 0; i < keymap.length; i++){
+                for(int j = 0; j < keymap[i].length(); j++){
+                    if(map.containsKey(keymap[i].charAt(j))){
+                        if( (j+1) < map.get(keymap[i].charAt(j))){
+                            map.replace(keymap[i].charAt(j),j+1);
+                        }
+                    } else {
+                        map.put(keymap[i].charAt(j),j+1);
+                    }
+                }
+            }
+
+            for (int i = 0; i < targets.length; i++){
+                int num = 0;
+                for ( int j = 0; j < targets[i].length(); j++){
+                    Integer mKey = map.get(targets[i].charAt(j));
+                    if( mKey != null){
+                        num += mKey;
+                    } else {
+                        num = -1;
+                        break;
+                    }
+                }
+                answer[i] = num;
+            }
+            return answer;
+        }
+    }
+}
