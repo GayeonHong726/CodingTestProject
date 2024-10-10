7927ec29021ce0fa3b7b48b7dafbeaf9ce913457
GayeonHong726 <1379hong@naver.com>
2024-10-10 23:45:54 +0900

    [24.10.10] Programmers12982(2)

--- src/basics/Programmers12982.java
+++ src/basics/Programmers12982.java
@@ -1,4 +1,24 @@
 package basics;
 
 public class Programmers12982 {
+    public static void main(String[] args){
+        int[] d = {1,3,2,5,4};
+        int budget = 9;
+
+        int sum = 0;
+        int answer = 0;
+
+        for(int i = 0; i < d.length; i++){
+            sum += d[i];
+            if(sum > budget){
+                answer = i;
+                break;
+            }
+        }
+        if (sum <= budget){
+            answer = d.length;
+        }
+
+        System.out.println(answer);
+    }
 }
