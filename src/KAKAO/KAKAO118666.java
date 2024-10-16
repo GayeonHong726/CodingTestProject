beaca72f56fba869ef15d7975945b31ce40caf7e
GayeonHong726 <1379hong@naver.com>
2024-10-16 23:28:04 +0900

    [24.10.16] KAKAO118666(1)

--- src/KAKAO/KAKAO118666.java
+++ src/KAKAO/KAKAO118666.java
@@ -4,5 +4,7 @@ public class KAKAO118666 {
     public static void main(String[] args){
         String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
         int[] choices = {5, 3, 2, 7, 5};
+
+
     }
 }
