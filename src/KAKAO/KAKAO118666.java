87c3a4493115add7a40a9ee2941001a40fcd3469
GayeonHong726 <1379hong@naver.com>
2024-10-17 19:40:58 +0900

    [24.10.17] KAKAO118666(2)

--- src/KAKAO/KAKAO118666.java
+++ src/KAKAO/KAKAO118666.java
@@ -1,10 +1,13 @@
 package KAKAO;
 
+import java.util.HashMap;
+
 public class KAKAO118666 {
     public static void main(String[] args){
         String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
         int[] choices = {5, 3, 2, 7, 5};
 
-
+        HashMap<String, Integer> kind = new HashMap<>();
+        int[] score = {3,2,1,0,1,2,3};
     }
 }
