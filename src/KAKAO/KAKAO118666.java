d75d4aa4798db30070e1533cd419c3a68eb888ce
GayeonHong726 <1379hong@naver.com>
2024-10-18 19:21:34 +0900

    [24.10.18] KAKAO118666(3)

--- src/KAKAO/KAKAO118666.java
+++ src/KAKAO/KAKAO118666.java
@@ -7,7 +7,38 @@ public class KAKAO118666 {
         String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
         int[] choices = {5, 3, 2, 7, 5};
 
-        HashMap<String, Integer> kind = new HashMap<>();
-        int[] score = {3,2,1,0,1,2,3};
+        char[] type = {'R','T','C','F','J','M','A','N'};
+        HashMap<Character, Integer> typeMap = new HashMap<>();
+
+        String answer = "";
+
+        for(Character t : type){
+            typeMap.put(t,0);
+        }
+
+        for(int i = 0; i<survey.length; i++){
+            char left = survey[i].charAt(0);
+            char right = survey[i].charAt(1);
+
+            if(choices[i] == 4){
+                continue;
+            } else if(choices[i] < 4){
+                typeMap.put(left, typeMap.get(left) + (4 - choices[i]));
+            } else {
+                typeMap.put(right, typeMap.get(right) + (choices[i] - 4));
+            }
+        }
+
+        for(int i = 0; i<type.length; i += 2){
+            char left = type[i];
+            char right = type[i+1];
+
+            if(typeMap.get(left) >= typeMap.get(right)){
+                answer += left;
+            }else {
+                answer += right;
+            }
+        }
+        System.out.println(answer);
     }
 }
