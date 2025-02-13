489a87aa616bacb6a7c12a7153e49117fd5694f0
GayeonHong726 <1379hong@naver.com>
2025-02-13 20:56:24 +0900

    [25.2.13] Programmers284529

---
+++ src/MYSQL/Programmers284529.java
@@ -0,0 +1,16 @@
+package MYSQL;
+
+public class Programmers284529 {
+//    1.
+//    SELECT d.DEPT_ID, d.DEPT_NAME_EN, ROUND(AVG(e.SAL)) AS 'AVG_SAL'
+//    FROM HR_DEPARTMENT d JOIN HR_EMPLOYEES e ON d.DEPT_ID = e.DEPT_ID
+//    GROUP BY DEPT_ID
+//    ORDER BY 3 DESC
+//
+//    2.
+//    SELECT d.DEPT_ID, d.DEPT_NAME_EN, ROUND(AVG(e.SAL)) AS 'AVG_SAL'
+//    FROM HR_DEPARTMENT d, HR_EMPLOYEES e
+//    WHERE d.DEPT_ID = e.DEPT_ID
+//    GROUP BY DEPT_ID
+//    ORDER BY 3 DESC
+}
