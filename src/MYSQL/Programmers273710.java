01504eda02dc67dd25c4d328f2a6fbdc0ff1a46d
GayeonHong726 <1379hong@naver.com>
2025-02-16 23:15:04 +0900

    [25.2.16] Programmers273710

---
+++ src/MYSQL/Programmers273710.java
@@ -0,0 +1,7 @@
+package MYSQL;
+
+public class Programmers273710 {
+//    SELECT I.ITEM_ID, I.ITEM_NAME
+//    FROM ITEM_INFO I JOIN ITEM_TREE T ON I.ITEM_ID = T.ITEM_ID
+//    WHERE T.PARENT_ITEM_ID IS NULL
+}
