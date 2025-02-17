3ec964730008cf12c23f7c575efddb6a26731bf8
GayeonHong726 <1379hong@naver.com>
2025-02-17 16:19:56 +0900

    [25.2.17] Programmers17680

---
+++ src/basics/Programmers17680.java
@@ -0,0 +1,29 @@
+package basics;
+import java.util.ArrayList;
+public class Programmers17680 {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        ArrayList<String> cache = new ArrayList<>();
+
+        if(cacheSize == 0){
+            return cities.length * 5;
+        }
+        for(String city : cities){
+            city = city.toUpperCase();
+
+            if(cache.contains(city)){
+                cache.remove(city);
+                cache.add(city);
+                answer++;
+            }
+            else {
+                if(cache.size() == cacheSize){
+                    cache.remove(0);
+                }
+                cache.add(city);
+                answer += 5;
+            }
+        }
+        return answer;
+    }
+}
