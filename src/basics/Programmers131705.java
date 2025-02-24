deb983c40f47c26377b9511b50b4e73d4e87d5ba
GayeonHong726 <1379hong@naver.com>
2025-02-24 23:10:40 +0900

    [25.2.24] Programmers131705(2)

--- src/basics/Programmers131705.java
+++ src/basics/Programmers131705.java
@@ -1,7 +1,19 @@
+package basics;
+
 public class Programmers131705 {
     class Solution {
         public int solution(int[] number) {
             int answer = 0;
+            int len = number.length;
+            for(int i = 0; i < len - 2; i++){
+                for(int j = i + 1; j < len - 1; j++){
+                    for(int t = j + 1; t < len; t++){
+                        if(number[i] + number[j] + number[t] == 0){
+                            answer++;
+                        }
+                    }
+                }
+            }
             return answer;
         }
     }
