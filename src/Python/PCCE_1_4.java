502ac45abb4a7d306690e8138803e287ff86fb8b
GayeonHong726 <1379hong@naver.com>
2025-02-25 16:02:49 +0900

    [25.2.25] PCCE_1_4_Python

---
+++ src/Python/PCCE_1_4.java
@@ -0,0 +1,58 @@
+package Python;
+
+public class PCCE_1_4 {
+//    1.
+//    message = "Let's go!"
+//
+//    print("3 \n 2 \n 1")
+//    print(message)
+//
+//    2.
+//    angle1 = int(input()) # 값 입력하기
+//    angle2 = int(input()) # 값 입력하기
+//
+//    sum_angle = angle1 + angle2
+//    print(sum_angle % 360) # 360 으로 나눈 값의 나머지를 구하기
+//
+//    3.
+//    # 풀이1. while문 사용
+//    number = int(input()) # 값 입력하기
+//    answer = 0
+//
+//    while number > 0: # while 문을 사용하여 number
+//    answer += number % 100
+//    number //= 100
+//
+//    print(answer)
+//
+//    # 풀이2. for문 사용
+//    number = int(input()) # 값 입력하기
+//    answer = 0
+//
+//    # str() : 문자열로 변환하기
+//    # len() : 문자열의 길이 반환하기
+//    # number의 길이를 계산함으로써 반복횟수 지정하기
+//    for i in range(len(str(number))//2):
+//    answer += number % 100
+//    number //= 100
+//
+//    print(answer)
+//
+//    4.
+//    code = input() # 값 입력하기
+//    # code[-4] : 뒤에서 4번째 글자 하나 가져오기
+//    # code[-4:] : 뒤에서부터 4글자 전부 가져오기
+//    # slicing 기능을 활용하여 문자열의 끝에서 4개의 문자 추출
+//            last_four_words = code[-4:]
+//
+//    if last_four_words == "_eye":
+//        print("Ophthalmologyc")
+//    elif last_four_words == "head":
+//        print("Neurosurgery")
+//    elif last_four_words == "infl":
+//        print("Orthopedics")
+//    elif last_four_words == "skin":
+//        print("Dermatology")
+//    else:
+//        print("direct recommendation")
+}
