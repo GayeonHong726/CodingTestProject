3c4a3dcd5d99993e4cdb246a88a4b4aec2f08524
GayeonHong726 <1379hong@naver.com>
2025-03-02 14:54:29 +0900

    [25.3.2] Programmers181914

---
+++ src/Python/Programmers181914.java
@@ -0,0 +1,15 @@
+package Python;
+
+public class Programmers181914 {
+//    def solution(number):
+//        answer = 0
+//
+//        for n in number:
+//            answer += int(n)
+//
+//        return answer % 9
+//
+//    # 제너레이터 표현식
+//    def solution(number):
+//        return sum(int(n) for n in number) % 9
+}
