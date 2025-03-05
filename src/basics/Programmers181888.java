0985318987d399d327a027f7085491aa277d4a0e
GayeonHong726 <1379hong@naver.com>
2025-03-06 00:10:09 +0900

    [25.3.6] Programmers181888_JAVA

---
+++ src/basics/Programmers181888.java
@@ -0,0 +1,43 @@
+package basics;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Programmers181888 {
+//    class Solution {
+//        public List solution(int[] num_list, int n) {
+//            List<Integer> answer = new ArrayList<>();
+//            for(int i = 0; i < num_list.length; i+=n){
+//                answer.add(num_list[i]);
+//            }
+//            return answer;
+//        }
+//    }
+//
+//    class Solution {
+//        public int[] solution(int[] num_list, int n) {
+//            List<Integer> list = new ArrayList<>();
+//            for(int i = 0; i < num_list.length; i+=n){
+//                list.add(num_list[i]);
+//            }
+//            return list.stream().mapToInt(Integer::intValue).toArray();
+//        }
+//    }
+//
+//    class Solution {
+//        public int[] solution(int[] num_list, int n) {
+//            List<Integer> list = new ArrayList();
+//
+//            for (int i = 0; i < num_list.length; i+=n) {
+//                list.add(num_list[i]);
+//            }
+//
+//            int[] answer = new int[list.size()];
+//
+//            for (int i = 0; i < list.size(); i++) {
+//                answer[i] = list.get(i);
+//            }
+//            return answer;
+//        }
+//    }
+}
