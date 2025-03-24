beee2ef0472911eae72e0b6055aff0b85d01e9d7
GayeonHong726 <1379hong@naver.com>
2025-03-24 22:36:01 +0900

    [25.3.24] Programmers340213

---
+++ src/basics/Programmers340213.java
@@ -0,0 +1,43 @@
+package basics;
+import java.util.*;
+
+public class Programmers340213 {
+    class Solution {
+        public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+            StringTokenizer st;
+            // 비디오 길이 구하기
+            st = new StringTokenizer(video_len,":");
+            int len = Integer.parseInt(st.nextToken()) * 60 + Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(pos,":");
+            int now = Integer.parseInt(st.nextToken()) * 60 + Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(op_start,":");
+            int start = Integer.parseInt(st.nextToken()) * 60 + Integer.parseInt(st.nextToken());
+
+
+            st = new StringTokenizer(op_end,":");
+            int end = Integer.parseInt(st.nextToken()) * 60 + Integer.parseInt(st.nextToken());
+
+            for(String command : commands){
+                if(start <= now && now <= end) now = end;
+                if(command.equals("prev")){
+                    now = (now - 10 <= 0) ? 0 : now - 10;
+                } else {
+                    now = (now + 10) >= len ? len : now + 10;
+                }
+            }
+            if(start <= now && now <= end) now = end;
+
+            //시간 변환
+            int hour = now / 60;
+            int minute = now % 60;
+            StringBuilder answer = new StringBuilder();
+            if(hour < 10) answer.append("0");
+            answer.append(hour).append(":");
+            if(minute < 10) answer.append("0");
+            answer.append(minute);
+
+            return answer.toString();
+        }
+    }
+}
