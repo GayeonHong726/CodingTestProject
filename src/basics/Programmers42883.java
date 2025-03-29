4b99d802dc70bf557b8e0359b0be79902c4efd68
GayeonHong726 <1379hong@naver.com>
2025-03-30 00:36:06 +0900

    [25.3.30] Programmers42883(2)

--- src/basics/Programmers42883.java
+++ src/basics/Programmers42883.java
@@ -24,6 +24,29 @@ public class Programmers42883 {
             }
 
             return sb.reverse().substring(0,len);
+
+//            Stack<Character> stack = new Stack<>();
+//            // String 을 char[] 배열로 변환하는 메서드
+//            for(char n : number.toCharArray()){
+//                while(!stack.isEmpty() && k > 0 && stack.peek() < n){
+//                    stack.pop();
+//                    k--;
+//                }
+//                stack.push(n);
+//            }
+//            // 제거해야 할 숫자가 남아있다면 스택의 뒤에서부터 제거!
+//            while(k-- > 0){
+//                stack.pop();
+//            }
+//
+//            StringBuilder result = new StringBuilder();
+//            // pop() 할 때만 LIFO 규칙이 적용!
+//            // 아래의 경우는 "저장된 순서" 그대로 접근
+//            for(char s : stack){
+//                result.append(s);
+//            }
+//            // StringBuilder 후에는 toString() 처리 필수!
+//            return result.toString();
         }
     }
 }
