ea0c0c9cd1bc7437ed56be5b049feb731a31be90
GayeonHong726 <1379hong@naver.com>
2025-03-31 20:46:39 +0900

    [25.3.31] Practice

---
+++ src/basics/Practice.java
@@ -0,0 +1,63 @@
+package basics;
+import java.util.Scanner;
+
+
+public class Practice {
+    public static void main(String[] args) {
+        // 구구단 출력하기
+//        {
+//            Scanner sc = new Scanner(System.in);
+//
+//            while (true) {
+//                // 입력 받기
+//                int s = sc.nextInt();
+//                int e = sc.nextInt();
+//
+//                // 유효성 검사
+//                if (s < 2 || s > 9 || e < 2 || e > 9) {
+//                    System.out.println("INPUT ERROR!");
+//                    continue; // 유효하지 않으면 다시 입력받기
+//                }
+//
+//                for (int i = 1; i <= 9; i++){
+//                    if(s <= e){
+//                        for(int j = s; j <= e; j++){
+//                            System.out.print(j + " * " + i + " = " + (j * i));
+//                            if(j != e){
+//                                System.out.print("   ");
+//                            }
+//                        }
+//                    } else {
+//                        for(int j = s; j >= e; j--){
+//                            System.out.print(j + " * " + i + " = " + (j * i));
+//                            if(j != e){
+//                                System.out.print("   ");
+//                            }
+//                        }
+//                    }
+//
+//                    System.out.println();
+//                }
+//
+//                break; // 프로그램 종료
+//            }
+//
+//            sc.close();
+//        }
+        // 문자열 거꾸로 출력
+//        {
+//            Scanner sc = new Scanner(System.in);
+//            String sen = sc.next();
+//
+//            StringBuilder rev = new StringBuilder(sen);
+//            System.out.println(rev.reverse().toString());
+//
+//            for(int i = sen.length() - 1; i >= 0; i--){
+//                System.out.print(sen.charAt(i));
+//            }
+//            System.out.println();
+//        }
+
+        //
+    }
+}
