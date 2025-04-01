45c7fc86ceb5ad4c9f4424b9382a82986cd31b2d
GayeonHong726 <1379hong@naver.com>
2025-04-01 23:59:06 +0900

    [25.4.1] StringDate

---
+++ src/MYSQL/StringDate.java
@@ -0,0 +1,25 @@
+package MYSQL;
+
+public class StringDate {
+//    SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+//        CASE
+//            WHEN STATUS = 'SALE' THEN '판매중'
+//            WHEN STATUS = 'RESERVED' THEN '예약중'
+//        ELSE '거래완료'
+//        END AS 'STATUS'
+//    FROM USED_GOODS_BOARD
+//    WHERE CREATED_DATE = '2022-10-05'
+//    ORDER BY 1 DESC
+//
+//    SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+//    FROM ANIMAL_INS
+//    WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+//    ORDER BY 1
+//
+//    SELECT CAR_ID,
+//        ROUND(AVG(DATEDIFF(END_DATE, START_DATE)+1),1) AS AVERAGE_DURATION
+//    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+//    GROUP BY CAR_ID
+//    HAVING AVERAGE_DURATION >= 7
+//    ORDER BY 2 DESC , 1 DESC
+}
