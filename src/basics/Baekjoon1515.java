0f94dd778cebd0e341e50d0d4f28e9aced05a7fb
GayeonHong726 <1379hong@naver.com>
2025-05-21 16:38:52 +0900

    [25.5.21] Baekjoon20920(2)

---
+++ src/basics/Baekjoon1515.java
@@ -0,0 +1,4 @@
+package basics;
+
+public class Baekjoon1515 {
+}
--- src/basics/Baekjoon20920.java
+++ src/basics/Baekjoon20920.java
@@ -1,58 +1,45 @@
 package basics;
 
+import java.io.*;
 import java.util.*;
 
 public class Baekjoon20920 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        // 빠른 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 빠른 출력
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        HashMap<String,Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
+        Map<String, Integer> map = new HashMap<>();
         for(int i = 0; i < N; i++){
-            String sen = sc.next();
-            if(sen.length() >= M){
-                // 외우자
-                map.put(sen, map.getOrDefault(sen,0) + 1);
+            String word = br.readLine();
+            if(word.length() >= M) {
+                map.put(word,map.getOrDefault(word,0) + 1);
             }
         }
-        sc.close();
 
         List<String> wordList = new ArrayList<>(map.keySet());
-        // 람다
-//        wordList.sort((a,b) -> {
-//            int freq = map.get(b) - map.get(a); // 빈도수 비교
-//            int len = b.length() - a.length(); // 길이 비교
-//            // 빈도가 같지않다면 빈도가 높은 순, 같다면 단어가 긴 순
-//            return freq != 0 ? freq : len;
-//        });
 
-        // 정렬
-        Collections.sort(wordList, new Comparator<String>(){
+        wordList.sort((a,b) -> {
+            int freq = map.get(b) - map.get(a);
+            if(freq != 0) return freq;
 
-            @Override
-            public int compare(String a, String b) {
-                int freq = map.get(b) - map.get(a); // 1. 빈도 비교
-                if(freq != 0) {
-                    return freq;
-                }
+            int len = b.length() - a.length();
+            if(len != 0) return len;
 
-                int len = b.length() - a.length(); // 2. 길이 비교
-                if(len != 0) {
-                    return len;
-                }
-
-                return a.compareTo(b); // 3. 알파벳 정렬
-            }
+            return a.compareTo(b);
         });
 
-        // 출력
         for(String word : wordList){
-            System.out.println(word);
+            bw.write(word);
+            bw.newLine();
         }
 
-
+        bw.flush();
+        bw.close();
     }
-
 }
