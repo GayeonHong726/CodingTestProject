4b970b40b933e10574e601480a18681c49fc881e
GayeonHong726 <1379hong@naver.com>
2025-05-21 17:34:54 +0900

    [25.5.21] Baekjoon1515

--- src/basics/Baekjoon1515.java
+++ src/basics/Baekjoon1515.java
@@ -1,4 +1,27 @@
 package basics;
 
+import java.io.*;
+
 public class Baekjoon1515 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String target = br.readLine();
+        int pt = 0;
+        int N = 1;
+
+        while(true){
+            String cur = Integer.toString(N);
+
+            for(int i = 0; i < cur.length(); i++){
+                if(pt < target.length() && target.charAt(pt) == cur.charAt(i)){
+                    pt++;
+                }
+                if(pt == target.length()){
+                    System.out.println(N);
+                    return;
+                }
+            }
+            N++;
+        }
+    }
 }
